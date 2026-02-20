51986a89f4072c37cdb186e592d0bc4096585b06
webophil <webophil@gmail.com>
2026-02-20 14:40:56 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -128,8 +128,8 @@ app.get('/', (req, res) => {
   </h1>
 
   <p class="hero-sub">
-    Vous jouez plusieurs plaques de 6 ou 12 cartons ? L'animateur va trop vite et le marquage devient difficile à suivre ? Vous bousculez un ou plusieurs cartons et les jetons sont déplacés ?
-    Votre solution : EZLoto gère tous vos cartons en temps réel en un seul clic, le marquage se fait tout seul, pour tous vos jeux. L'appli vous prévient dès qu'un carton "touche" à un numéro près, et vous annonce votre victoire avec visualisation du carton gagnant. vous n'avez plus qu'à faire vérifier le carton physique !
+    <ul><li>Vous jouez plusieurs plaques de 6 ou 12 cartons ?</li><li>L'animateur va trop vite et le marquage devient difficile à suivre ?</li><li>Vous bousculez un ou plusieurs cartons et les jetons sont déplacés ?</li></ul>
+    Votre solution : EZLoto gère tous vos cartons en temps réel, le marquage se fait en un seul clic, pour tous vos jeux. L'appli vous prévient dès qu'un carton "touche" à un numéro près, et vous annonce votre victoire avec visualisation du carton gagnant. vous n'avez plus qu'à faire vérifier le carton physique !
   </p>
 
   <div class="cta-group">
